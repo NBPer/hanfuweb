910661c267034ecd5d25fe4ee08f1a42ff15f15c
sj <sj@qq.com>
2020-10-04 17:53:51 +0800

    项目搭建初始化，整合log4j2

--- src/main/java/com/jzt/controller/UserController.java
+++ src/main/java/com/jzt/controller/UserController.java
@@ -1,21 +1,29 @@
 package com.jzt.controller;
 
+import com.jzt.entity.UserEntity;
 import com.jzt.service.IUserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    private static Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
 
     @Autowired
     private IUserService userService;
 
     @RequestMapping("/findAll")
     public String findAll(){
-        System.out.println("表示层：查询所有用户");
-
+        logger.info("表示层：查询所有用户");
+        List<UserEntity> userEntityList = userService.findAll();
+        logger.info("result : " + userEntityList);
         return "userlist";
     }
 }
